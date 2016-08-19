0149e1050c4e15fdfcda1ad467d6520797a73c73
Alexander Brandstedt <alexanderbrandstedt@gmail.com>
2016-08-19 12:12:02 +0200

    adding main file to hold cli

---
+++ prom_finagle_exporter/main.py
@@ -0,0 +1,27 @@
+import click
+from prom_finagle_exporter.handler import falcon_app
+from prom_finagle_exporter.discovery import register_consul
+
+
+@click.group(help='')
+def cli():
+    pass
+
+
+@click.command()
+@click.option('-s', '--service', help='service name', required=True, type=str)
+@click.option('-u', '--url', help='url to collect from', required=True, type=str)
+@click.option('-p', '--port', help='', required=True, type=int)
+@click.option('-c', '--consul-host', help='', default='', type=str)
+def start(service, url, port, consul_host):
+    if consul_host:
+        register_consul(consul_host, port)
+
+    falcon_app(url, service, port=port)
+
+
+cli.add_command(start)
+
+
+if __name__ == '__main__':
+    cli()
