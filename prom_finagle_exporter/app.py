3eb83b762a0c56e263fa83dead0abab9c3985388
Alexander Brandstedt <alexanderbrandstedt@gmail.com>
2016-08-19 12:09:01 +0200

    move falcon funcs and classes to handler file

--- prom_finagle_exporter/app.py
+++ prom_finagle_exporter/app.py
@@ -1,41 +1,8 @@
 import click
 import socket
-import falcon
 import consul
 
-import prometheus_client
-from wsgiref import simple_server
-
-from prom_finagle_exporter.prom import TwitterFinagleCollector
-
-
-class metricHandler:
-    def __init__(self, url='', service=''):
-        self._service = service
-        self._url = url
-
-    def on_get(self, req, resp):
-        resp.set_header('Content-Type', prometheus_client.exposition.CONTENT_TYPE_LATEST)
-        registry = TwitterFinagleCollector(self._url, self._service)
-        collected_metric = prometheus_client.exposition.generate_latest(registry)
-        resp.body = collected_metric
-
-
-class healthHandler:
-    def __init__(self):
-        pass
-
-    def on_get(self, req, resp):
-        resp.body = '{"status": "OK"}'
-
-
-def falcon_app(url, service, port=9161, addr='0.0.0.0'):
-    api = falcon.API()
-    api.add_route('/health', healthHandler())
-    api.add_route('/', metricHandler(url=url, service=service))
-
-    httpd = simple_server.make_server(addr, port, api)
-    httpd.serve_forever()
+from prom_finagle_exporter.handler import falcon_app
 
 
 def get_ip_address():
